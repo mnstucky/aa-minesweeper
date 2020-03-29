9edf9d8bda525ef05ccd0f812f7f684bc9535161
Matt Stucky <mnstucky@gmail.com>
2020-03-29 17:18:01 -0500

    Add input validation

--- game.rb
+++ game.rb
@@ -32,9 +32,27 @@ class Game
 
     def process_move
         input = self.get_move
+        input = self.validate_move(input)
         position = input.split(" ")
         position.map! { |num| num.to_i }
         position
+    end
+
+    def validate_move(input)
+        valid = false
+        digits = "012345678"
+        while valid == false
+            valid = true 
+            valid = false unless input.length == 3
+            valid = false unless digits.include?(input[0])
+            valid = false unless input[1] == " "
+            valid = false unless digits.include?(input[2])
+            if valid == false
+                print "Wrong input. Try again: "
+                input = gets.chomp
+            end
+        end
+        input
     end
 
 end
