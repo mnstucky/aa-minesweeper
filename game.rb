c023162581a67326eb408b1d2ec0995cca43a7a5
Matt Stucky <mnstucky@gmail.com>
2020-03-30 11:28:47 -0500

    Add load/save functionality

--- game.rb
+++ game.rb
@@ -1,18 +1,45 @@
 require_relative 'tile'
 require_relative 'board'
-require 'byebug'
+require 'yaml'
 
 class Game
 
     def initialize
-        @board = Board.new
+        choice = self.get_start
+        if choice == "l"
+            self.load_game
+            self.play
+        else
+            @board = Board.new
+            self.play
+        end
+    end
+
+    def get_start
+        print "Welcome to minesweeper! Would you like to load a game (type 'l') or start a new game (type 'n')?"
+        input = gets.chomp
+        input = self.validate_start(input)
+        input
+    end
+    
+    def validate_start(input)
+        valid = false
+        while valid == false
+            valid = true if input == "l" || input == "n"
+            if valid == false
+                print "Wrong input. Try again: "
+                input = gets.chomp
+            end
+        end
+        input
     end
 
     def play
         while !@board.won
             system('clear')
             @board.render
-            if self.make_choice == 'r'
+            choice = self.make_choice
+            if choice == "r"
                 position = self.get_position
                 if @board.lost(position)
                     puts "Sorry, you chose a bomb. Game over!"
@@ -23,15 +50,17 @@ class Game
                         puts "You win!"
                     end
                 end
-            else
+            elsif choice == "f"
                 position = self.get_position
                 @board.mark_flag(position)
+            else
+                self.save_game
             end
         end
     end
 
     def make_choice
-        print "Would you like to reveal a tile (type 'r') or flag a bomb (type 'f')? "
+        print "Would you like to reveal a tile (type 'r'), flag a bomb (type 'f'), or save your game (type 's')? "
         input = gets.chomp
         input = self.validate_choice(input)
         input
@@ -40,7 +69,7 @@ class Game
     def validate_choice(input)
         valid = false
         while valid == false
-            valid = true if input == "r" || input == "f"
+            valid = true if input == "r" || input == "f" || input == "s"
             if valid == false
                 print "Wrong input. Try again: "
                 input = gets.chomp
@@ -75,8 +104,15 @@ class Game
         input
     end
 
-    def flag_bomb
+    def save_game
+        serialized_board = YAML::dump(@board)
+        file = File.open("savegame.yml", "w")
+        file.puts serialized_board
+        file.close
+    end
 
+    def load_game
+        @board = YAML.load_file("savegame.yml")
     end
 
 end
@@ -84,4 +120,3 @@ end
 #code for testing
 
 g = Game.new
-g.play
